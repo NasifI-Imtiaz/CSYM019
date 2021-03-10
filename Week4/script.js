97a1f9b4b0322adc3c052c179797826856f6aebe
NasifI-Imtiaz <nasif.imtiaz007@gmail.com>
2021-03-10 10:33:14 +0000

    My 4th Commit Week4

---
+++ Week4/script.js
@@ -0,0 +1,22 @@
+function x() 
+
+{
+ alert('Script loaded');
+}
+
+function y() 
+
+{
+ var e = 1;
+ var f = 0;
+ var g = e + f;
+ console.log (g);
+ console.log (g+1);
+console.log( g+2);
+console.log (g+3);
+console.log( g+4);
+
+}
+//calling functions
+y();
+y();
